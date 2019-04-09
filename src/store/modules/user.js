a948e21a7fd00e126d2840b1b8ae953532686ec7
zdj <695898986@qq.com>
2019-04-09 17:39:09 +0800

    waht

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -34,6 +34,8 @@ const actions = {
     const { username, password } = userInfo
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
+        debugger
+        console.log(response)
         const { data } = response
         commit('SET_TOKEN', data.token)
         setToken(data.token)
