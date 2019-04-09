eedc8a6794beaddab56bf528ead8b83a99c07923
zdj <695898986@qq.com>
2019-04-09 17:14:00 +0800

    waht

---
+++ src/views/wechat/menu.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="components-container">
+    <wechat-menu />
+  </div>
+</template>
+
+<script>
+import WechatMenu from '@/components/WechatMenu/index'
+
+export default {
+  name: 'Menu',
+  components: { WechatMenu }
+}
+</script>
+
+<style scoped>
+
+</style>
