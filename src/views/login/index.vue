bd1a078c8217c824b9e0d388d6a7443e996b7460
zdj <695898986@qq.com>
2019-04-09 14:42:00 +0800

    55

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -22,7 +22,7 @@
           auto-complete="on"
         />
       </el-form-item>
-
+      <!---->
       <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
         <el-form-item prop="password">
           <span class="svg-container">
