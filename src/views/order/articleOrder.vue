d9472a8a6d80dae6a6825760cbe7ca6095876789
zdj <695898986@qq.com>
2019-05-23 16:55:24 +0800

    article

--- src/views/order/articleOrder.vue
+++ src/views/order/articleOrder.vue
@@ -1,29 +1,43 @@
 <template>
   <div class="app-container">
+    <div class="filter-container">
+      <el-input v-model="listQuery.title" :placeholder="$t('table.title')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-select v-model="listQuery.status" placeholder="选择订单状态" clearable style="width: 90px" class="filter-item">
+        <el-option v-for="item in orderType" :key="item.value" :label="item.label" :value="item.value" />
+      </el-select>
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+        {{ $t('table.search') }}
+      </el-button>
+    </div>
+
     <el-table v-loading="listLoading" :data="list" border :fit="fitWidth" size="small" stripe highlight-current-row style="width: 100%">
+
+      <el-table-column type="expand">
+        <template slot-scope="props">
+          <el-form label-position="left" inline class="demo-table-expand">
+            <el-form-item label="OPENID">
+              <span>{{ props.row.openid }}</span>
+            </el-form-item>
+            <el-form-item label="用户姓名">
+              <span>{{ props.row.userName === null|| props.row.userName ===''? '暂无': props.row.userName }}</span>
+            </el-form-item>
+            <el-form-item label="电话号">
+              <span>{{ props.row.phone === null||props.row.phone === ''? '暂无': props.row.phone }}</span>
+            </el-form-item>
+            <el-form-item label="文章单价">
+              <span>{{ props.row.article.pay_price }}</span>
+            </el-form-item>
+          </el-form>
+        </template>
+      </el-table-column>
       <el-table-column align="center" label="文章标题" width="240px">
         <template slot-scope="scope">
           <span>{{ scope.row.article_name }}</span>
         </template>
       </el-table-column>
-      <el-table-column width="120px" align="center" label="微信昵称">
+      <el-table-column width="120px" align="center" label="下单微信昵称">
         <template slot-scope="scope">
           <span>{{ scope.row.user.nickname }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column width="120px" align="center" label="OPENID">
-        <template slot-scope="scope">
-          <span>{{ scope.row.openid }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column width="120px" align="center" label="用户姓名">
-        <template slot-scope="scope">
-          <span>{{ scope.row.userName }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column width="120px" align="center" label="phone">
-        <template slot-scope="scope">
-          <span>{{ scope.row.phone }}</span>
         </template>
       </el-table-column>
       <el-table-column width="120px" align="center" label="订单价格">
@@ -33,7 +47,7 @@
       </el-table-column>
       <el-table-column width="120px" align="center" label="文章单价">
         <template slot-scope="scope">
-          <span>{{ scope.row.pay_price }}</span>
+          <span>{{ scope.row.article.pay_price }}</span>
         </template>
       </el-table-column>
       <el-table-column width="120px" align="center" label="难度">
@@ -54,9 +68,9 @@
           </el-tag>
         </template>
       </el-table-column>
-      <el-table-column width="180px" align="center" label="发布时间">
+      <el-table-column width="180px" align="center" label="下单时间">
         <template slot-scope="scope">
-          <span>{{ scope.row.deploy_time | parseUTCtime }}</span>
+          <span>{{ scope.row.order_time }}</span>
           <!--<span>{{ scope.row.deploy_time | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>-->
         </template>
       </el-table-column>
@@ -75,18 +89,15 @@
         </template>
       </el-table-column>-->
 
-      <el-table-column align="left" label="Actions" width="100px">
+      <el-table-column align="left" label="订单操作" width="100px">
         <template slot-scope="scope">
-          <div>
-            <router-link style="display: inline-block" :to="'/article/edit/'+scope.row.id">
-              <el-button type="primary" size="small" icon="el-icon-edit" circle />
-            </router-link>
-            <el-button v-show="scope.row.status!==1" type="danger" size="small" icon="el-icon-delete" circle @click="handleModifyStatus(scope.row)" />
-          </div>
+          <!--退款-->
+          <el-tooltip class="item" effect="dark" content="退款" placement="top-start">
+            <el-button v-show="scope.row.status === 2" type="success" icon="el-icon-check" size="small" circle @click="handleDeleteOrder(scope.row)" />
+          </el-tooltip>
         </template>
       </el-table-column>
     </el-table>
-
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
   </div>
 </template>
@@ -163,19 +174,48 @@ export default {
       listLoading: true,
       listQuery: {
         page: 1,
-        limit: 20
-      }
+        limit: 10,
+        status: '',
+        article_name: '',
+        openid: ''
+      },
+      orderType: [
+        {
+          label: '新创建',
+          value: 0
+        },
+        {
+          label: '预支付创建',
+          value: 1
+        },
+        {
+          label: '已支付',
+          value: 2
+        },
+        {
+          label: '申请退款中',
+          value: 3
+        },
+        {
+          label: '退款完成',
+          value: 4
+        }
+      ]
     }
   },
   created() {
     this.getList()
   },
   methods: {
+    handleDeleteOrder() {
+
+    },
     getList() {
       this.listLoading = true
       fetchArticleOrderList(this.listQuery).then(response => {
-        this.list = response.data.docs
-        this.total = response.data.total
+        this.list = response.data.data.docs
+        this.total = response.data.data.total
+
         this.listLoading = false
       })
     }
@@ -191,5 +231,22 @@ export default {
     position: absolute;
     right: 15px;
     top: 10px;
+  }
+  .demo-table-expand {
+    font-size: 12px;
+    width: 100%;
+  }
+  .demo-table-expand label {
+    width: 50px;
+    color: #99a9bf;
+    font-size: 14px;
+  }
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 60%;
+  }
+  .demo-table-expand .el-form-item span{
+    font-size: 10px;
   }
 </style>
